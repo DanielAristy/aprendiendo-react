ecc4e414b4c6cbe1cf8c45f8e21fad15dec83b50
DanielAristy <danielaristy22@gmail.com>
2021-06-29 15:18:36 -0500

    Manejo de estados utlizando el hook de react useState, en este ejemplo se utliza el incremento y decremento de un valor en la web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncreaseClick = () => {
+    setCount(count + 1);
+  }
+
+  const handleDecreaseClick = () => {
+    setCount(count - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <p>Contador {count}</p>
+      <button onClick={handleIncreaseClick}>
+        Aumentar
+      </button>
+      <button onClick={handleDecreaseClick}>
+        Disminuir
+      </button>
+    </Fragment>
   );
 }
 
